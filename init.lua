d74347c23f5775c5ae7f1c0d6b1fa3d8bc8207d0
Brandon Corey <bcorey3660@gmail.com>
2024-08-30 01:55:55 -0400

    add avante

--- init.lua
+++ init.lua
@@ -883,6 +883,73 @@ require('lazy').setup({
     lazy = false,
   },
 
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    lazy = false,
+    opts = {
+      -- add any opts here
+    },
+    keys = {
+      {
+        '<leader>aa',
+        function()
+          require('avante.api').ask()
+        end,
+        desc = 'avante: ask',
+        mode = { 'n', 'v' },
+      },
+      {
+        '<leader>ar',
+        function()
+          require('avante.api').refresh()
+        end,
+        desc = 'avante: refresh',
+      },
+      {
+        '<leader>ae',
+        function()
+          require('avante.api').edit()
+        end,
+        desc = 'avante: edit',
+        mode = 'v',
+      },
+    },
+    dependencies = {
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to setup it properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
