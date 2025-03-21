0d27078a7e4a55f857eea173ff369cd609420741
Brandon Corey <bcorey3660@gmail.com>
2025-03-21 16:32:42 -0400

    add avante to config

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -195,6 +195,9 @@ return {
         'diff',
         'html',
         'lua',
+        'javascript',
+        'typescript',
+        'python',
         'luadoc',
         'markdown',
         'markdown_inline',
@@ -219,9 +222,7 @@ return {
     'kylechui/nvim-surround',
     version = '*',
     event = 'VeryLazy',
-    config = function()
-      require('nvim-surround').setup {}
-    end,
+    opts = {},
   },
 
   {
@@ -298,5 +299,88 @@ return {
         keymaps = keymaps.bind_super_maven(),
       }
     end,
+  },
+
+  {
+    -- Make sure to set this up properly if you have lazy=true
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      file_types = { 'markdown', 'Avante' },
+    },
+    ft = { 'markdown', 'Avante' },
+  },
+
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = 'copilot',
+      copilot = {
+        model = 'o3-mini',
+      },
+      vendors = {
+        copilot_gemini_flash = {
+            __inherited_from = 'copilot',
+            model = 'gemini-2.0-flash-001',
+          },
+        copilot_claude_sonnet = {
+            __inherited_from = 'copilot',
+            model = 'claude-3.7-sonnet',
+          },
+      },
+      -- gemini = {
+      --   model = 'gemini-2.0-flash', -- your desired model (or use gpt-4o, etc.)
+      --   timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+      --   temperature = 0,
+      --   max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+      --   --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      -- },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'echasnovski/mini.pick', -- for file_selector provider mini.pick
+      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      'ibhagwan/fzf-lua', -- for file_selector provider fzf
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- Available
+          behaviour = {
+            auto_suggestions = false, -- Experimental stage
+            auto_set_highlight_group = true,
+            auto_set_keymaps = true,
+            support_paste_from_clipboard = false,
+            minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+            enable_token_counting = true, -- Whether to enable token counting. Default to true.
+            enable_cursor_planning_mode = false,
+          },
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+    },
   },
 }
